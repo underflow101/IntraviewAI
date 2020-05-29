75b94ea153f846cf76762734f3c1e6e8cdb6712d
underflow101 <ikarus125@gmail.com>
2020-05-29 11:36:37 +0900

    update
    Signed-off-by: underflow101 <ikarus125@gmail.com>

--- api/views.py
+++ api/views.py
@@ -7,4 +7,12 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse, JsonResponse
 from datetime import datetime
+
+@api_view(['GET'])
+def ping(request):
+    if request.method == 'GET':
+        return JsonResponse({
+            "message" : "ok"
+        })
